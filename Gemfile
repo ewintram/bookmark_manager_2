e019e244dec7eab046ec9b6bae5c4460d13f4419
Nikolaos Sakellarios <lunaticnick@hotmail.com>
2017-12-13 15:38:07 +0000

    Changes

--- Gemfile
+++ Gemfile
@@ -2,11 +2,16 @@ source 'https://rubygems.org'
 
 ruby '2.4.2'
 
-gem 'rspec'
 gem 'data_mapper'
 gem 'pry'
 gem 'dm-postgres-adapter'
 gem 'dm-migrations'
-gem 'capybara'
 gem 'sinatra'
 gem 'database_cleaner'
+gem 'pg'
+
+
+group :test do
+  gem "capybara"
+  gem "rspec"
+end
--- app/models/db_helper.rb
+++ app/models/db_helper.rb
@@ -1,7 +1,7 @@
 require 'data_mapper'
 
 def db_initialize
-  DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+  DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
   DataMapper.finalize
   DataMapper.auto_upgrade!
 end
